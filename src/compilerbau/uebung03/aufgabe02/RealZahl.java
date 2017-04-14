66ff44ff16e790d70f77181d37d50c1c53a69cdd
Pgin-Oth <pgin.oth@gmail.com>
2017-04-14 23:14:36 +0200

    CompilerBau Uebung03.

--- src/compilerbau/uebung03/aufgabe02/RealZahl.java
+++ src/compilerbau/uebung03/aufgabe02/RealZahl.java
@@ -32,29 +32,36 @@ public class RealZahl {
     private boolean isNumberFromOnToNine(char c) {
         return c >= 49 && c <= 57;
     }
+    private boolean isExponent(char c) {
+        return c ==69 || c == 101;
+    }
 
     private boolean isPoint(char c) {
         return c == 46;
+    }
+
+    private boolean isPlusOrMinus(char c) {
+        return c == 43 || c == 45;
     }
 
     private boolean start() {
         if (this.isEndOfInput())
             return false;
         char c = this.getChar();
-        if (this.isPoint(c))
+        if (this.isNumber(c))
             return this.firstState();
-        else if (this.isZero(c))
-            return this.secondState();
-        else if (this.isNumberFromOnToNine(c))
-            return this.thirdState();
         return false;
     }
 
     private boolean firstState() {
         if (this.isEndOfInput())
-            return false;
+            return true;
         char c = this.getChar();
         if (this.isNumber(c))
+            return this.firstState();
+        else if (this.isPoint(c))
+            return this.secondState();
+        else if (this.isExponent(c))
             return this.fourthState();
         return false;
     }
@@ -63,28 +70,48 @@ public class RealZahl {
         if (this.isEndOfInput())
             return false;
         char c = this.getChar();
-        if (this.isPoint(c))
-            return this.fourthState();
+        if (this.isNumber(c))
+            return this.thirdState();
         return false;
     }
 
     private boolean thirdState() {
         if (this.isEndOfInput())
-            return false;
+            return true;
         char c = this.getChar();
         if (this.isNumber(c))
             return this.thirdState();
-        else if (this.isPoint(c))
+        else if (this.isExponent(c))
             return this.fourthState();
         return false;
     }
 
     private boolean fourthState() {
         if (this.isEndOfInput())
+            return false;
+        char c = this.getChar();
+        if (this.isNumber(c))
+            return this.sixthState();
+        if (this.isPlusOrMinus(c))
+            return this.fifthState();
+        return false;
+    }
+
+    private boolean fifthState() {
+        if (this.isEndOfInput())
+            return false;
+        char c = this.getChar();
+        if (this.isNumber(c))
+            return this.sixthState();
+        return false;
+    }
+
+    private boolean sixthState() {
+        if (this.isEndOfInput())
             return true;
         char c = this.getChar();
         if (this.isNumber(c))
-            return this.fourthState();
+            return this.sixthState();
         return false;
     }
 
@@ -93,15 +120,19 @@ public class RealZahl {
     }
 
     public static void main(String args[]) {
-        System.out.println(" ist" + (new RealZahl(".").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("..").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("0").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("0.1.").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("0.1a1").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("a.11").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("0.").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl("0.0").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl(".1").validator() ? "" : " nicht") + " ein Fixkommazahl.");
-        System.out.println(" ist" + (new RealZahl(".11").validator() ? "" : " nicht") + " ein Fixkommazahl.");
+        System.out.println(" ist" + (new RealZahl(".").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl(".E1").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("1.").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0.E1").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0E").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0E+").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0E-").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0a0").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("01").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0.0").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("01.01").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0.0123E456").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("0.0123E-456").validator() ? "" : " nicht") + " ein Realzahl.");
+        System.out.println(" ist" + (new RealZahl("01.0123E+456").validator() ? "" : " nicht") + " ein Realzahl.");
     }
 }
